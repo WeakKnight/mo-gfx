8b63ed9817a05be010d6307448337ea2a6e2da15
Tianyu Li <ltyucb@gmail.com>
2020-03-12 02:40:55 -0600

    Update app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -72,7 +72,7 @@ void App::Init()
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 	
-	m_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	m_window = glfwCreateWindow(WIDTH, HEIGHT, "mo-gfx", nullptr, nullptr);
 
 	glfwSetFramebufferSizeCallback(m_window, framebufferResizeCallback);
 
