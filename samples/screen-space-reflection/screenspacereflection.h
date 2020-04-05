d3a401c21094e638bc92ef4b8dfbb6e3b826234a
Tianyu Li <ltyucb@gmail.com>
2020-04-05 02:53:21 -0600

    Init Sample SSR

---
+++ samples/screen-space-reflection/screenspacereflection.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "app.h"
+
+class ScreenSpaceReflectionExample: public App
+{
+private:
+	void Init() override;
+
+	void MainLoop() override;
+
+	void CleanUp() override;
+};
+
